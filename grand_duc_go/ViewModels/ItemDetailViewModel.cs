73e134ed1e0947e0c888872528f5ac13292e102a
Louis Campbell <campbell.earl.louis@xanlosh.us>
2020-02-12 07:50:33 +0100

    Project Setup

---
+++ grand_duc_go/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using grand_duc_go.Models;
+
+namespace grand_duc_go.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
